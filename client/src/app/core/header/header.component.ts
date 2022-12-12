0c9ea932dde691f67b7033cea1717ef5dba0654e
Angel Kasabov <ankasabov142@gmail.com>
2022-12-12 22:55:26 +0200

    update header responsiveness

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  private cartItems: number = 0;
+  private favItems: number = 0;
+  private screenWidth: number = window.innerWidth;
+
+  get getCartItems(): number | null {
+    return this.cartItems === 0
+      ? null
+      : this.cartItems;
+  }
+
+  get getFavItems(): number | null {
+    return this.favItems === 0
+      ? null
+      : this.favItems;
+  }
+
+  get isMobile(): boolean {
+    return this.screenWidth < 1000;
+  }
+
+  ngOnInit(): void {
+    this.cartItems = Math.round(Math.random() * 10); //todo
+    this.favItems = Math.round(Math.random() * 10); //todo
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.screenWidth = window.innerWidth;
+  }
 }
